16cf71fe10bdad1af78739929744f83c0e81a336
tdd <2578741722@qq.com>
2026-05-09 23:04:37 +0800

    gateway

--- genproto/user/user.pb.go
+++ genproto/user/user.pb.go
@@ -26,10 +26,10 @@ var File_user_user_proto protoreflect.FileDescriptor
 
 const file_user_user_proto_rawDesc = "" +
 	"\n" +
-	"\x0fuser/user.proto\x12\x04user\x1a\x13common/common.proto\x1a\x15api/annotations.proto2\xb2\x01\n" +
-	"\vUserService\x12E\n" +
+	"\x0fuser/user.proto\x12\x04user\x1a\x13common/common.proto\x1a\x15api/annotations.proto2\xcc\x01\n" +
+	"\vUserService\x12_\n" +
 	"\n" +
-	"CreateUser\x12\x19.common.CreateUserRequest\x1a\x1a.common.CreateUserResponse\"\x00\x12\\\n" +
+	"CreateUser\x12\x19.common.CreateUserRequest\x1a\x1a.common.CreateUserResponse\"\x1a\x82\xd3\xe4\x93\x02\x14:\x01*\"\x0f/v1/user/create\x12\\\n" +
 	"\vGetUserInfo\x12\x1a.common.GetUserInfoRequest\x1a\x17.common.GetUserResponse\"\x18\x82\xd3\xe4\x93\x02\x12:\x01*\"\r/v1/user/info2O\n" +
 	"\rStreamService\x12>\n" +
 	"\tListValue\x12\x15.common.SimpleRequest\x1a\x16.common.StreamResponse\"\x000\x012K\n" +
